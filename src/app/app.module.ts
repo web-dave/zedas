dc0cd0ebd6b29147f771979602c5dc0f81d8abba
Dave <dave@webdave.de>
2019-04-17 15:03:33 +0200

    ng add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MoinComponent } from './moin/moin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoinComponent
   ],
   imports: [
     BrowserModule
