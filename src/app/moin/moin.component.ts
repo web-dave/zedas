aee2acaa9455d75720da0503983db93a13edbac3
Dave <dave@webdave.de>
2019-04-17 15:08:05 +0200

    moin component

--- src/app/moin/moin.component.ts
+++ src/app/moin/moin.component.ts
@@ -1,20 +1,31 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewEncapsulation,
+  Input,
+  Output,
+  EventEmitter,
+  OnChanges
+} from '@angular/core';
 
 @Component({
-  selector: 'app-moin',
+  selector: 'moin-moin',
   template: `
-    <p>
-      moin works!
-    </p>
+    <p>moin {{ name }}!</p>
+    <button (click)="hi()">^5</button>
   `,
-  styles: [],
   encapsulation: ViewEncapsulation.ShadowDom
 })
-export class MoinComponent implements OnInit {
+export class MoinComponent implements OnChanges {
+  @Input() name: string;
+  @Output() highFive = new EventEmitter<string>();
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnChanges(obj) {
+    console.warn(obj);
   }
 
+  hi() {
+    this.highFive.emit(`Hi! I'm ${this.name}!`);
+  }
 }
